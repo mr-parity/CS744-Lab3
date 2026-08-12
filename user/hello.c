942988aec1175782ee8ef28893d7dd90117cf64b
mr-parity <trushildhokiya@gmail.com>
2026-08-12 21:27:57 +0530

    added hello usr cmd, [prints hello world]

---
+++ user/hello.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h" // important along side user.h as user.h uses types in types.h (SELF NOTE)
+#include "user/user.h"
+
+int main()
+{
+    // print
+    printf("Hello, World! \n");
+    
+    exit(0);
+}
